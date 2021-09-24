572b84fb8a9311b3b4a374787b512cabd09123e2
Lisanne Nurk <lnruk@tktk.ee>
2021-09-24 13:04:45 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis;
+            //"Oled sündinud aastal {2001}"
+            
+            Console.WriteLine("Palun, sisesta oma vanus.");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - userAge;
+
+            //string interpolation
+
+            Console.WriteLine($"Oled sündinud {yearOfBirth} aastal.");     
+        }
+    }
+}
